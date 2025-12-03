a81d16e5454623762005ef44136164bb61116c6a
Fredrick Lani <31032310+laniricky@users.noreply.github.com>
2025-12-03 15:06:51 +0300

    Change fade-out implementation to window.onload
    Replaced DOMContentLoaded event listener with window.onload to apply fade-out class.

--- public/res.js
+++ public/res.js
@@ -1,10 +1,3 @@
-
-    document.addEventListener("DOMContentLoaded", function () {
-        document.body.style.transition = "opacity 10s";
-        document.body.style.opacity = "1";
-
-        setTimeout(() => {
-            document.body.style.opacity = "0";
-        }, 100); // slight delay to ensure the transition applies
-    });
-
+ window.onload = () => {
+        document.body.classList.add("fade-out");
+    }
