0550f63709b261b08bea0c16e5ced1ffd388f7a0
Fredrick Lani <31032310+laniricky@users.noreply.github.com>
2025-12-03 15:05:01 +0300

    Remove script tags from res.js
    Removed unnecessary script tags from res.js.

--- public/res.js
+++ public/res.js
@@ -1,4 +1,4 @@
-<script>
+
     document.addEventListener("DOMContentLoaded", function () {
         document.body.style.transition = "opacity 10s";
         document.body.style.opacity = "1";
@@ -7,4 +7,4 @@
             document.body.style.opacity = "0";
         }, 100); // slight delay to ensure the transition applies
     });
-</script>
+
